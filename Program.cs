ff0afac5d873f11060578b2c21ed12e3d866acd6
yoshua1265 <yoshua1265@hotmail.com>
2026-06-23 10:29:55 -0600

    Agregando repositorio

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// ============================================================
+// SISTEMA DE INVENTARIO - Clase 1.1
+// Estado: Mensaje de bienvenida
+// ============================================================
+
+
+
+using System.Reflection;
+
+var assembly  = Assembly.GetExecutingAssembly();
+var Version = assembly.GetName().Version;
+
+
+Console.WriteLine("==========================================");
+Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"Versión {Version}");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine("Estado: Proyecto inicializado");
+
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Sistema de Gestion de Inventario
+
+Proyecto del curso *Fundamentos de .NET* 
+
+## Requisitos
+- .Net 10 SDK
+
+## Como ejecutar
+
+dotnet run
+
+
+## Estructura del proyecto
+
+InventarioApp/
+- Program.cs # Punto de entrada
+- InventarioApp.csproj # Configuracion
+- .gitignore # Archivos ignorados por Git
+- src/
+
+## Progreso del curso
+- [x] Modulo 1: El ecosistema .NET
